141ef7749e3d7a8eb101b21d623ed7e507dd7f39
daviroo <david.borthwick@gmail.com>
2022-10-30 10:12:04 +0000

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-require(`dotenv`).config()
+require(`dotenv`).config();
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 /**
  * @type {import('gatsby').GatsbyConfig}
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `David Borthwick Software`,
+    siteTitleAlt: `David Borthwick Software - Profile Site`,
+    siteHeadline: `David Borthwick Software - Profile Site`,
+    siteUrl: `https://borthwicksoftware.com`,
+    siteDescription: `A profile site for David Borthwick Software`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@daviroo`,
   },
   plugins: [
     {
@@ -24,32 +24,32 @@ module.exports = {
       // See the theme's README for all available options
       options: {},
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
-        start_url: `/`,
-        background_color: `#141821`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#f6ad55`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Cara - @lekoarts/gatsby-theme-cara`,
+    //     short_name: `Cara`,
+    //     description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    //     start_url: `/`,
+    //     background_color: `#141821`,
+    //     // This will impact how browsers show your PWA/website
+    //     // https://css-tricks.com/meta-theme-color-and-trickery/
+    //     // theme_color: `#f6ad55`,
+    //     display: `standalone`,
+    //     icons: [
+    //       {
+    //         src: `/android-chrome-192x192.png`,
+    //         sizes: `192x192`,
+    //         type: `image/png`,
+    //       },
+    //       {
+    //         src: `/android-chrome-512x512.png`,
+    //         sizes: `512x512`,
+    //         type: `image/png`,
+    //       },
+    //     ],
+    //   },
+    // },
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
@@ -59,4 +59,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
